7b59171ef3dbbee9a4f71038fca5bdc072064d69
EmmellaBeatrice <danahbeat@gmail.com>
2020-06-29 13:41:04 +0300

    Initial commit

--- app/src/main/java/com/example/sunshine/MainActivity.java
+++ app/src/main/java/com/example/sunshine/MainActivity.java
@@ -34,7 +34,6 @@ public class MainActivity extends AppCompatActivity {
         ListView listView = (ListView) findViewById(R.id.list);
 
         listView.setAdapter(adapter);
-
     }
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
